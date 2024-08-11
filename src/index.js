bedc06f7c4dcda2b428d018c9eb27a5b3d0f94d3
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-08-11 10:19:05 +0530

    created first configuration file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const {PORT} = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.listen(PORT,()=>{
+        console.log(`Server started at port: ${PORT}`);
+    })
+}
+
+setupAndStartServer();
