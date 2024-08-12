f7dea59aadb802c36aa7e31d9aef7697ca5516a5
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-08-12 19:46:04 +0530

    Completed city repository for now

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -7,6 +7,7 @@ class CityRepository {
             const city = await City.create({ name });
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer!"); // ? temporarily
             throw { error };
         }
     }
@@ -17,7 +18,31 @@ class CityRepository {
                     id: cityId,
                 },
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository layer!"); // ? temporarily
+            throw { error };
+        }
+    }
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId,
+                },
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer!"); // ? temporarily
+            throw { error };
+        }
+    }
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer!"); // ? temporarily
             throw { error };
         }
     }
