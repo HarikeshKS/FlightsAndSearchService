0ede330f10d331a81c0a23a51bb648cb08f0c333
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-08-13 23:48:45 +0530

    Added Airplane model and migrations

---
+++ src/migrations/20240813102944-create-airplane.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airplanes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      model_number: {
+        type: Sequelize.STRING,
+        allowNull:false
+      }, 
+      capacity: {
+        type: Sequelize.DOUBLE,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airplanes');
+  }
+};
